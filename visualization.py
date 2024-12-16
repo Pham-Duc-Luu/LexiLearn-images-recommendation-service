f44a3da831d056e8ff46e0590f0a156c6dd5ef6c
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-12-16 20:49:35 +0700

    add def description

--- visualization.py
+++ visualization.py
@@ -1,13 +1,25 @@
 import os
-from IPython.display import display, Image, HTML
+
+import matplotlib
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+matplotlib.use('TkAgg')
 
 
 def display_result(list_scores, img_dir_path="./datasets/flickr8k/Images"):
+    """
+        Display the images in Pycharm backend
+    :param list_scores: list of image path - score pairs
+    :param img_dir_path: the path to the image directory
+    :return:
+    """
     for img, score in list_scores:
         img_path = os.path.join(img_dir_path, img)
         try:
-            display(Image(filename=img_path))
-            display(HTML(f"<p style='text-align: center; font-size: 16px;'>{img}</p>"))
-        except:
+            img = mpimg.imread(img_path)
+            plt.imshow(img)
+            plt.show()
+        except FileNotFoundError:
             print("Words or relating images not existing!")
             continue
